0f2df81d2563f3fe26ea20758b4313431b9dcf59
Abderrahman Khial <94490420+Abderrahmankhial@users.noreply.github.com>
2022-12-22 04:11:11 -0800

    Update LZ77.py

--- LZ77.py
+++ LZ77.py
@@ -3,7 +3,8 @@
 import os
 from tkinter import Tk
 from tkinter import filedialog
-from art import tprint
+import art
+import text2art
 #1. find_in_dict function is used to find the longest substring in the dictionary that matches the buffer   
 
 def find_in_dict(buffer, dictionary):
@@ -69,92 +70,106 @@ def decompress(compressed_message):
             message += part
     return message
 #function that opens file manager top select a file with multi selection desabled
-def open_file():
+def open_file(title):
     root = Tk()
     root.withdraw()
     root.wm_attributes('-topmost', 1)
-    file=filedialog.askopenfilename( title="Choose the file you want ", filetypes =(("Text", "*.txt"),("All Files","*.*"),("Text", "*.txt")))
+    file=filedialog.askopenfilename(title=title,filetypes=[("Text Files", "*.txt")])
     if file=="":
         return "no file selected.error"
     root.update()
     root.destroy()
     return open(file, "r").read()
 #function that saves the compressed file
-def save_file(compressed_message, Title , file="output.txt"):
-    root = Tk()
+def save_file(compressed_message, Title ,):
+    root = Tk()#create a window
     root.withdraw()
-    root.wm_attributes('-topmost', 1)
-    file=filedialog.asksaveasfilename( title=Title, filetypes =(("Text", "*.txt"),("All Files","*.*"),("Text", "*.txt")))
-    if file=="":
+    root.wm_attributes('-topmost', 1)#make the window appear on top of all other windows
+    file=filedialog.asksaveasfilename(title=Title,filetypes=[("Text Files", "*.txt")])#save the file as a text file
+    if file=="":#if the user didn't select a file, save the file in the same directory as the program under the name output.txt
         file="output.txt"
     root.update()
     root.destroy()
-    open(file, "w").write(str(compressed_message))
-def select_file():
+    open(file, "w").write(str(compressed_message))#write the compressed message to the file
+#function that selects a file and checks if the file is empty or too big or if the user didn't select a file
+def select_file(title):
     is_selected=False
     while is_selected!= True:
-            message = open_file()
-            if message=="":#if the file is empty, exit the program
+            message = open_file(title)
+            if message=="":#if the file is empty
+                print("The file is empty, select another file")
                 pass
-            elif len(message)>1000000:
+            elif len(message)>1000000:#if the file is too big
+                print("The file is too big, choose another file")
                 pass
-            elif message=="no file selected.error":
+            elif message=="no file selected.error":#if the user didn't select a file
+                print("No file selected, Select a file")
                 pass
-            elif message!="":
+            elif message!="":#if the file is valid exit the loop and return the message
                 is_selected=True
                 return message
 #4. main function is used to run the code
 if __name__ == "__main__":
-    print("\033[1;32m")#set the color of the text to green
-    tprint("LZ77", font="block", chr_ignore=True)#print the title
-    choice=input("Do you want to compress a file or decompress a file or both? (c/d/b) ")#get the choice from the user
-    if choice=="b":
-        #open the file
-        message = select_file()
-        #compress the file
-        try:
-            #get the buffer size and the dictionary size from the user
-            buffer, dictionary = input("Enter the buffer size and the dictionary size respectively seperated by space: ").split()
-            print("buffer size: " + buffer + " dictionary size: " + dictionary)
-            compressed_message = compress(message, int(buffer), int(dictionary))
-        except:
-            #if the user enters invalid input, use the default values
-            print("Invalid input, using default values")
-            compressed_message = compress(message, 4, 8)
-        #save the compressed file
-        save_file(compressed_message, "Choose the file you want to save the compressed file to")
-        #decompress the file
-        decompressed_message = decompress(compressed_message)
-        #print the results
-        print("Original message: " + message)
-        print("Compressed message: " + str(compressed_message))
-        print("Decompressed message: " + decompressed_message)
-        print("Are the messages equal? " + str(message == decompressed_message))
-    elif choice=="c":
-        #open the file
-        message = select_file()
-        #compress the file
-        try: 
-            buffer, dictionary = input("Enter the buffer size and the dictionary size respectively seperated by space: ").split()
-            print("buffer size: " + buffer + " dictionary size: " + dictionary)
-            compressed_message = compress(message, int(buffer), int(dictionary))
-        except:
-            print("Invalid input, using default values")
-            compressed_message = compress(message, 4, 8)
-        save_file(compressed_message, "Save the compressed file")
-    elif choice=="d":
-        #open the file
-        message = select_file()
-        #convert the string to a list
-        x=eval(message)
-        #decompress the file
-        decompressed_message = decompress(x)
-        #print the results
-        print("Decompressed message: " + decompressed_message)
-        save= input("Do you want to save the decompressed message? (y/n) ")
-        if save=="y":
-            save_file(decompressed_message, "Save the decompressed file")
+    try:
+        print("\033[1;32m")#set the color of the text to green
+        art.tprint("LZ77", font="block", chr_ignore=True)#print the title
+        choice=input("Do you want to compress a file or decompress a file or both? (c/d/b) ")#get the choice from the user
+        if choice=="b":
+            #open the file
+            message = select_file("Choose the file you want to compress")
+            #compress the file
+            try:
+                #get the buffer size and the dictionary size from the user
+                buffer, dictionary = input("Enter the buffer size and the dictionary size respectively seperated by space: ").split()
+                print("buffer size: " + buffer + " dictionary size: " + dictionary)
+                compressed_message = compress(message, int(buffer), int(dictionary))#compress the file
+            except:
+                #if the user enters invalid input, use the default values
+                print("Invalid input, using default values")
+                compressed_message = compress(message, 4, 8)
+            #save the compressed file
+            save_file(compressed_message, "Choose the file you want to save the compressed file to")
+            #decompress the file
+            decompressed_message = decompress(compressed_message)
+            #print the results
+            print("Original message: " + message)
+            print("Compressed message: " + str(compressed_message))
+            print("Decompressed message: " + decompressed_message)
+            print("Are the messages equal? " + str(message == decompressed_message))
+        elif choice=="c":
+            #open the file
+            message = select_file("Choose the file you want to compress")
+            #compress the file
+            try: 
+                buffer, dictionary = input("Enter the buffer size and the dictionary size respectively seperated by space: ").split()
+                print("buffer size: " + buffer + " dictionary size: " + dictionary)
+                compressed_message = compress(message, int(buffer), int(dictionary))
+            except:
+                print("Invalid input, using default values")
+                compressed_message = compress(message, 4, 8)
+            save_file(compressed_message, "Save the compressed file")
+        elif choice=="d":
+            #open the file
+            message = select_file("Choose the file you want to decompress")
+            #convert the string to a list
+            x=eval(message)
+            #decompress the file
+            decompressed_message = decompress(x)
+            #print the results
+            print("Decompressed message: " + decompressed_message)
+            save= input("Do you want to save the decompressed message? (y/n) ")
+            if save=="y":
+                save_file(decompressed_message, "Save the decompressed file")
+            else:
+                exit()
         else:
+            print("Invalid input")
+    except KeyboardInterrupt:
+        x=input("\nDo you want to exit? (y/n) ")
+        if x=="y":
+            out=art.text2art("Goodbye",font="rnd-small")
+            print(out)
             exit()
-    else:
-        print("Invalid input")
+        else:
+            pass
+        
